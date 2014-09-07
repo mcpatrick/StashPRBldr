41ed9fb92dbb9baf56e227f4de17862f99bba781
Curt Patrick <cpatrick@netflix.com>
2014-09-07 10:40:07 -0700

    First halting steps toward adding a trigger class.

---
+++ src/main/java/org/jenkinsci/plugins/StashPRBuilder/StashPRBuilderTrigger.java
@@ -0,0 +1,9 @@
+package org.jenkinsci.plugins.StashPRBuilder;
+
+import hudson.triggers.Trigger;
+
+/**
+ * Created by cpatrick on 9/3/14.
+ */
+public class StashPRBuilderTrigger extends Trigger {
+}
